686879e3d1ef8a1459b55a7c732b06d73097efd3
Sorelul <sorinake_eu@yahoo.com>
2025-03-12 14:45:25 +0200

    Enhance Order and OrderItem components to integrate useFetcher for ingredient loading; improve loading state display and refactor markup for better readability

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,6 +1,6 @@
 // Test ID: IIDSAT
 
-import { useLoaderData } from 'react-router-dom';
+import { useFetcher, useLoaderData } from 'react-router-dom';
 import { getOrder } from '../../services/apiRestaurant';
 import OrderItem from './OrderItem';
 import {
@@ -8,9 +8,20 @@ import {
   formatCurrency,
   formatDate,
 } from '../../utilities/helpers';
+import { useEffect } from 'react';
 
 function Order() {
   const order = useLoaderData();
+
+  const fetcher = useFetcher();
+
+  useEffect(
+    function () {
+      if (!fetcher.data && fetcher.state === 'idle') fetcher.load('/menu');
+    },
+    [fetcher],
+  );
+
   const {
     id,
     status,
@@ -23,23 +34,23 @@ function Order() {
   const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
   return (
-    <div className="space-y-8 px-4 py-6">
+    <div className="px-4 py-6 space-y-8">
       <div className="flex flex-wrap items-center justify-between gap-2">
         <h2 className="text-xl font-semibold">Order #{id} status</h2>
 
         <div className="space-x-2">
           {priority && (
-            <span className="rounded-full bg-red-500 px-3 py-1 text-sm uppercase tracking-wide text-red-50">
+            <span className="px-3 py-1 text-sm tracking-wide uppercase bg-red-500 rounded-full text-red-50">
               Priority
             </span>
           )}
-          <span className="rounded-full bg-green-500 px-3 py-1 text-sm uppercase tracking-wide text-green-50">
+          <span className="px-3 py-1 text-sm tracking-wide uppercase bg-green-500 rounded-full text-green-50">
             {status} order
           </span>
         </div>
       </div>
 
-      <div className="flex flex-wrap items-center justify-between gap-2 bg-stone-200 px-6 py-5">
+      <div className="flex flex-wrap items-center justify-between gap-2 px-6 py-5 bg-stone-200">
         <p className="font-medium">
           {deliveryIn >= 0
             ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
@@ -50,13 +61,21 @@ function Order() {
         </p>
       </div>
 
-      <ul className="divide-y divide-stone-200 border-b border-t">
+      <ul className="border-t border-b divide-y divide-stone-200">
         {cart.map((item) => (
-          <OrderItem key={item.id} item={item} />
+          <OrderItem
+            key={item.pizzaId}
+            item={item}
+            isLoadingIngredients={fetcher.state === 'loading'}
+            ingredients={
+              fetcher?.data?.find((el) => el.id === item.pizzaId)
+                ?.ingredients ?? []
+            }
+          />
         ))}
       </ul>
 
-      <div className="space-y-2 bg-stone-200 px-6 py-5">
+      <div className="px-6 py-5 space-y-2 bg-stone-200">
         <p className="text-sm font-medium text-stone-600">
           Price pizza: {formatCurrency(orderPrice)}
         </p>
